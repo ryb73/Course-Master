815e50e59a05c1393722ab958bf65f4dd0e9b0f3
Graham <shortstuffsushi@aim.com>
2012-04-09 17:50:15 -0500

    Quick fix for demo

--- src/com/coursemaster/service/Discussion.java
+++ src/com/coursemaster/service/Discussion.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.coursemaster.auth.Session;
 import com.coursemaster.auth.Session.Role;
@@ -82,41 +84,59 @@ public class Discussion extends AbstractService {
     }
 
     private void getBoards(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JSONObject rsp = DatabaseConnectionManager.executeQuery(String.format(
+              "select board.id, board.name, count(topic.id) topics " +
+              "from discussion_board board " +
+              "left join discussion_topic topic on topic.board = board.id " +
+              "where board.course = %d " +
+              "group by board.id, board.name",
+              Integer.parseInt(request.getParameter("courseId"))));
+
         try {
-            Connection connection = DatabaseConnectionManager.getConnection();
-            Statement statement = connection.createStatement();
-
-            String sqlStatement = String.format(
-                    "select board.id, board.name, count(topic.id) topics " +
-                    "from discussion_board board " +
-                    "left join discussion_topic topic on topic.board = board.id " +
-                    "where board.course = %d " +
-                    "group by board.id, board.name",
-                    Integer.parseInt(request.getParameter("courseId")));
-            statement.execute(sqlStatement);
-
-            StringBuilder jsonResult = new StringBuilder("{ status: 'OK', boards:[");
-            ResultSet resultSet = statement.getResultSet();
-            while(resultSet.next()) {
-                jsonResult.append("{id:").append(resultSet.getInt("id"))
-                    .append(",name:'").append(resultSet.getString("name"))
-                    .append("',topicCount:").append(resultSet.getInt("topics")).append("}");
-
-                if(!resultSet.isLast()) {
-                    jsonResult.append(",");
-                }
+            if (rsp.getInt("count") > 0) {
+                response.setStatus(HttpServletResponse.SC_OK);
             }
+            else {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        } catch (JSONException e) { }
+        response.getWriter().write(rsp.toString());
 
-            jsonResult.append("] }");
-
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setContentType("application/json");
-            response.getWriter().write(jsonResult.toString());
-        } catch(SQLException e) {
-            logger.error("An error occurred while getting list of boards: " + e.getMessage());
-
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
+//        try {
+//            Connection connection = DatabaseConnectionManager.getConnection();
+//            Statement statement = connection.createStatement();
+//
+//            String sqlStatement = String.format(
+//                    "select board.id, board.name, count(topic.id) topics " +
+//                    "from discussion_board board " +
+//                    "left join discussion_topic topic on topic.board = board.id " +
+//                    "where board.course = %d " +
+//                    "group by board.id, board.name",
+//                    Integer.parseInt(request.getParameter("courseId")));
+//            statement.execute(sqlStatement);
+//
+//            StringBuilder jsonResult = new StringBuilder("{ status: 'OK', boards:[");
+//            ResultSet resultSet = statement.getResultSet();
+//            while(resultSet.next()) {
+//                jsonResult.append("{id:").append(resultSet.getInt("id"))
+//                    .append(",name:'").append(resultSet.getString("name"))
+//                    .append("',topicCount:").append(resultSet.getInt("topics")).append("}");
+//
+//                if(!resultSet.isLast()) {
+//                    jsonResult.append(",");
+//                }
+//            }
+//
+//            jsonResult.append("] }");
+//
+//            response.setStatus(HttpServletResponse.SC_OK);
+//            response.setContentType("application/json");
+//            response.getWriter().write(jsonResult.toString());
+//        } catch(SQLException e) {
+//            logger.error("An error occurred while getting list of boards: " + e.getMessage());
+//
+//            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//        }
     }
 
     private void getMain(HttpServletRequest request, HttpServletResponse response) throws IOException {
